9211492f0b9bd409448bde0eb88859592ff23859
caremao <55214626+caremao@users.noreply.github.com>
2025-07-11 11:05:48 -0500

    Add REST API controller skeleton with Swagger

---
+++ api-rest/src/test/java/com/caremao/codebreaker/api/ApiRestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.caremao.codebreaker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
