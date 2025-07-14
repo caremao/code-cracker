bf2246516b4224b458f7183cb1f9ab88ab2a6a89
caremao <caremao@gmail.com>
2025-07-14 10:29:00 -0500

    Adding scan packages for rest app.

--- api-rest/src/main/java/com/caremao/codebreaker/api/ApiRestApplication.java
+++ api-rest/src/main/java/com/caremao/codebreaker/api/ApiRestApplication.java
@@ -3,7 +3,10 @@ package com.caremao.codebreaker.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.caremao.codebreaker.api",
+        "com.caremao.codebreaker.core"
+})
 public class ApiRestApplication {
 
     public static void main(String[] args) {
